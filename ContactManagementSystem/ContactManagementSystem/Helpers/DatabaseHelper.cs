856528205e02a72f456946bf4b685c03bb82924a
VeOS1127 <omethdasanayaka@gmail.com>
2026-05-29 07:21:50 +0530

    Database

--- ContactManagementSystem/ContactManagementSystem/Helpers/DatabaseHelper.cs
+++ ContactManagementSystem/ContactManagementSystem/Helpers/DatabaseHelper.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Data.SqlClient;
 
 namespace ContactManagementSystem.Helpers
 {
-    internal class DatabaseHelper
+    internal static class DatabaseHelper
     {
+        private static readonly string connectionString = @"Server=(localdb)\MSSQLLocalDB;Database=ContactsDB;Integrated Security=true;TrustServerCertificate=true;";
     }
 }
